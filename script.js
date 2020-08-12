1bf9fcf92124bd264d5fb6c20560243a68c61da4
John Wells <molekewl2017@gmail.com>
2020-08-11 19:43:24 -0700

    create setAttribute function - DRY

--- script.js
+++ script.js
@@ -8,19 +8,37 @@ const loader = document.getElementById('loader');
 
 let photosArray = [];
 
+function setAttributes(element, attributes) {
+  for (const key in attributes) {
+    element.setAttribute(key, attributes[key]);
+  }
+}
+
 // Create Elements Links/Photos
 function displayPhotos() {
   photosArray.forEach((photo) => {
-    // console.log(photo.links.html);
     // <a> to link unsplash
     const item = document.createElement('a');
-    item.setAttribute('href', photo.links.html);
-    item.setAttribute('target', '_blank');
+
+    // item.setAttribute('href', photo.links.html);
+    // item.setAttribute('target', '_blank');
+    setAttributes(item, {
+      href: photo.links.html,
+      target: 'blank'
+    })
+
     // <img> for photo
     const img = document.createElement('img');
-    img.setAttribute('src', photo.urls.regular);
-    img.setAttribute('alt', photo.alt_description);
-    img.setAttribute('title', photo.alt_description);
+
+    // img.setAttribute('src', photo.urls.regular);
+    // img.setAttribute('alt', photo.alt_description);
+    // img.setAttribute('title', photo.alt_description);
+    setAttributes(img, {
+      src: photo.urls.regular,
+      alt: photo.alt_description,
+      title: photo.alt_description
+    })
+
     // Img inside <a>, then inside image container
     item.appendChild(img);
     imageContainer.appendChild(item);
